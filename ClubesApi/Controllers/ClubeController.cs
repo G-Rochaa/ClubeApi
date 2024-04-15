92ab037ed49c01203f0f59d86ed1d93c26e9f945
Gustavo R. <agect.gustavo@gmail.com>
2024-04-14 22:03:34 -0300

    Adicionando o projeto com 1 modelo (clube) e 1 controlador (clubecontroller)

---
+++ ClubesApi/Controllers/ClubeController.cs
@@ -0,0 +1,32 @@
+﻿using ClubesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace ClubesApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClubeController : ControllerBase
+{
+    private static int id = 0;                  //Começamos nosso id em zero.
+    private static List<Clube> clubes = new List<Clube>();      //Criando a lista de clubes 
+
+    [HttpPost]                                                  //Post pois adicionaremos um clube
+    public void adicionaClube([FromBody] Clube clube)
+    {
+        clube.Id = id++;
+        clubes.Add(clube);                                     //Adicionando clube a uma lista de clubes
+
+    }
+
+    [HttpGet]
+    public IEnumerable<Clube> LeClubes()                       //Recuperando um IEnumeravel de clubes com todos os clubes cadastrados
+    {
+        return clubes;
+    }
+
+    
+
+  
+
+}
---
+++ ClubesApi/Models/Clube.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ClubesApi.Models;
+
+public class Clube
+{
+
+    [Required(ErrorMessage = "O nome é obrigatório!")] 
+    public string Nome { get; set; }
+    [Required]
+    public int Idade { get; set; }
+    [Required]
+    public string PresidenteAtual { get; set; }
+    [Required]
+    public int QntdTitulosNacionais { get; set; }
+    [Required]
+    public int QntdTitulosInternacionais { get; set; }
+}
